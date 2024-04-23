60868e984d8fb06e11a52efef5ee174cd4b7acbd
internet-unable <polocatic@gmail.com>
2024-04-23 16:37:23 +0300

    Indent update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import AuthInputs from './components/AuthInputs.jsx';
 import Header from './components/Header.jsx';
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <AuthInputs />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <AuthInputs />
+            </main>
+        </>
+    );
 }
