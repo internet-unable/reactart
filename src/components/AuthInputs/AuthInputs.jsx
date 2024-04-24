bc5548b5faba0c22cdb292ce9adb796feca8239a
internet-unable <polocatic@gmail.com>
2024-04-24 14:47:27 +0300

    Update for AuthInputs
    Finished with change of all standart class names into Tailwind class names

--- src/components/AuthInputs/AuthInputs.jsx
+++ src/components/AuthInputs/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import Input from '../Input/Input.jsx';
 import Button from '../Button/Button.jsx';
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,23 +23,23 @@ export default function AuthInputs() {
     const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
     return (
-        <div id="auth-inputs">
-            <Container>
+        <div id="auth-inputs" className="w-full max-w-sm mx-auto p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+            <div className="flex flex-col gap-2 mb-6">
                 <Input
                     label="Email"
                     type="email"
-                    $invalid={emailNotValid}
+                    invalid={emailNotValid}
                     onChange={(event) => handleInputChange('email', event.target.value)}
                 />
                 <Input
                     label="Password"
                     type="password"
-                    $invalid={emailNotValid}
+                    invalid={passwordNotValid}
                     onChange={(event) =>handleInputChange('password', event.target.value)}
                 />
-            </Container>
-            <div className="actions">
-                <button type="button" className="text-button">
+            </div>
+            <div className="flex justify-end gap-4">
+                <button type="button" className="text-amber-400 hover:text-amber-500">
                     Create a new account
                 </button>
                 <Button onClick={handleLogin}>Sign In</Button>
