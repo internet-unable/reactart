dc5d5f5a15516f5c8d1a867a87da5c05db6eb04b
internet-unable <polocatic@gmail.com>
2024-04-24 14:01:33 +0300

    Update for Header and AuthInputs. Created Button
    Learned how to use media queries and pseudo selectors, using Tailwind

--- src/components/AuthInputs/AuthInputs.jsx
+++ src/components/AuthInputs/AuthInputs.jsx
@@ -1,27 +1,13 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 import Input from '../Input/Input.jsx';
+import Button from '../Button/Button.jsx';
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
     margin-bottom: 1.5rem;
-`;
-
-const Button = styled.button`
-    padding: 1rem 2rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    border-radius: 0.25rem;
-    color: #1f2937;
-    background-color: #f0b322;
-    border-radius: 6px;
-    border: none;
-
-    &:hover {
-        background-color: #f0920e;
-    }
 `;
 
 export default function AuthInputs() {
